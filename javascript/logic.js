cab35cf1e248d96a7a2bf636c5653026b137cc73
oliverjessner <oliver.jessner@gmail.com>
2022-08-18 08:00:48 +0200

    wip js ep2

---
+++ javascript/logic.js
@@ -0,0 +1,9 @@
+console.log('Hallo Oli, Welt');
+console.log('Hallo Oli, Welt');
+console.log('Hallo Oli, Welt');
+console.log('Hallo Oli, Welt');
+console.log('Hallo Oli, Welt'); // ; ist am ende immer wichtig
+/*
+Mehrzeiligen Kommentar
+Eingeben
+*/ 
