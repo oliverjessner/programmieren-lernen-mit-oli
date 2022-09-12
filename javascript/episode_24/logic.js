02a1eeeb060b4c4add38404412de0f40556958ca
oliverjessner <oliver.jessner@gmail.com>
2022-09-12 16:28:46 +0200

    done js23&js24

--- javascript/episode_24/logic.js
+++ javascript/episode_24/logic.js
@@ -1,1 +1,24 @@
 'use strict';
+const address = 'https://jsonplaceholder.typicode.com/';
+
+const postsProm = fetch(`${address}posts/`, {
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json'
+    }
+}).then(resp => resp.json());
+const commentsProm = fetch(`comments/`, {
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json'
+    }
+}).then(resp => resp.json());
+
+Promise.allSettled([postsProm, commentsProm]).then(function (responses) {
+    console.log(responses);
+    return responses.filter(d => d.status === 'fulfilled').map(d => d.value)
+}).then(function (data) {
+    console.log(data);
+}).catch(function (err) {
+    console.log(err);
+});
