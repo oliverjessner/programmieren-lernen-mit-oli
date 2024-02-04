34b1d4c28c1f186e4b2f9e4bf5ce8428b3af9113
oliverjessner <oliver.jessner@gmail.com>
2024-02-04 16:18:17 +0100

    bruv change

--- nodejs/episode_14/frontend/main.js
+++ nodejs/episode_14/frontend/main.js
@@ -1,7 +1,7 @@
 import Keycloak from 'keycloak-js';
 
 const keycloak = new Keycloak({
-    url: 'http://0.0.0.0:8080/',
+    url: 'http://localhost:8080/',
     realm: 'myrealm',
     clientId: 'myapp',
 });
