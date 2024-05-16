0c27350d93ea8692471c053de33c00e517f64407
oliverjessner <oliver.jessner@gmail.com>
2024-05-16 15:10:21 +0200

    new ep 17:00

---
+++ einfuehrung_in_die_funktionale_programmierung/episode_3/impure_functions.js
@@ -0,0 +1,9 @@
+let counter = 0;
+
+function incrementCounter() {
+    counter++;
+    return counter;
+}
+
+console.log(incrementCounter());
+console.log(incrementCounter());
---
+++ einfuehrung_in_die_funktionale_programmierung/episode_3/pure_functions.js
@@ -0,0 +1,18 @@
+const length = 10;
+const width = 10;
+
+function rectangleArea(length, width) {
+    return length * width;
+}
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * 10) + 1;
+}
+
+console.log(rectangleArea(length, width));
+console.log(rectangleArea(length, width));
+console.log(rectangleArea(length, width));
+console.log(rectangleArea(length, width));
+console.log(rectangleArea(length, width));
+console.log(rectangleArea(length, width));
+console.log(rectangleArea(length, width));
