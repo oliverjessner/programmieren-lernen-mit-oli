9a8fb755a26a6f6ae16345d8776f1c6acfcccdc3
oliverjessner <oliver.jessner@gmail.com>
2024-09-04 13:00:50 +0200

    new tt

---
+++ tiktoks/rotate_matrix/step_by_step.js
@@ -0,0 +1,18 @@
+const matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+];
+/*
+Es bedeutet, die Zeilen und Spalten einer Matrix miteinander zu vertauschen.
+*/
+const transpose = [
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+];
+const output = [
+    [7, 4, 1],
+    [8, 5, 2],
+    [9, 6, 3],
+];
